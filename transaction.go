87b922706fa46c5d721ad8602a375488d7cd24ad
Grzegorz Lisowski <glisowski@fractalsoft.org>
2024-02-16 14:34:10 +0100

    Generate XRechnung XML with hardcoded data
    Here we create the structure based on the examples and specyfication
    from the xeinkauf[1] webpage and comparing it with the EN 16931[2]
    specyfication for FacturX 1.03 and ZUGFeRD 2.0.1
    
    [1] https://xeinkauf.de/xrechnung/versionen-und-bundles/
    [2] https://portal3.gefeg.com/projectdata/invoice/deliverables/installed/publishingproject/zugferd%202.0.1%20-%20facturx%201.03/en%2016931%20%E2%80%93%20facturx%201.03%20%E2%80%93%20zugferd%202.0.1%20-%20basic%20wl.scm/html/de/021.htm?https://portal3.gefeg.com/projectdata/invoice/deliverables/installed/publishingproject/zugferd%202.0.1%20-%20facturx%201.03/en%2016931%20%E2%80%93%20facturx%201.03%20%E2%80%93%20zugferd%202.0.1%20-%20basic%20wl.scm/html/de/026.htm

---
+++ transaction.go
@@ -0,0 +1,241 @@
+package xinvoice
+
+// Transaction defines the structure of the transaction in the CII standard
+type Transaction struct {
+	Lines      []*Line     `xml:"ram:IncludedSupplyChainTradeLineItem"`
+	Agreement  *Agreement  `xml:"ram:ApplicableHeaderTradeAgreement"`
+	Delivery   *Delivery   `xml:"ram:ApplicableHeaderTradeDelivery"`
+	Settlement *Settlement `xml:"ram:ApplicableHeaderTradeSettlement"`
+}
+
+// Line defines the structure of the IncludedSupplyChainTradeLineItem in the CII standard
+type Line struct {
+	ID              string           `xml:"ram:AssociatedDocumentLineDocument>ram:LineID"`
+	Name            string           `xml:"ram:SpecifiedTradeProduct>ram:Name"`
+	NetPrice        string           `xml:"ram:SpecifiedLineTradeAgreement>ram:NetPriceProductTradePrice>ram:ChargeAmount"`
+	TradeDelivery   *Quantity        `xml:"ram:SpecifiedLineTradeDelivery>ram:BilledQuantity"`
+	TradeSettlement *TradeSettlement `xml:"ram:SpecifiedLineTradeSettlement"`
+}
+
+// Quantity defines the structure of the quantity with its attributes for the CII standard
+type Quantity struct {
+	Amount   string `xml:",chardata"`
+	UnitCode string `xml:"unitCode,attr"`
+}
+
+// TradeSettlement defines the structure of the SpecifiedLineTradeSettlement of the CII standard
+type TradeSettlement struct {
+	TaxType        string `xml:"ram:ApplicableTradeTax>ram:TypeCode"`
+	TaxCode        string `xml:"ram:ApplicableTradeTax>ram:CategoryCode"`
+	TaxRatePercent string `xml:"ram:ApplicableTradeTax>ram:RateApplicablePercent"`
+	Sum            string `xml:"ram:SpecifiedTradeSettlementLineMonetarySummation>ram:LineTotalAmount"`
+}
+
+// Agreement defines the structure of the ApplicableHeaderTradeAgreement of the CII standard
+type Agreement struct {
+	BuyerReference string  `xml:"ram:BuyerReference"`
+	Seller         *Seller `xml:"ram:SellerTradeParty"`
+	Buyer          *Buyer  `xml:"ram:BuyerTradeParty"`
+}
+
+// Seller defines the structure of the SellerTradeParty of the CII standard
+type Seller struct {
+	Name                      string                     `xml:"ram:Name"`
+	LegalOrganization         *LegalOrganization         `xml:"ram:SpecifiedLegalOrganization"`
+	Contact                   *Contact                   `xml:"ram:DefinedTradeContact"`
+	PostalTradeAddress        *PostalTradeAddress        `xml:"ram:PostalTradeAddress"`
+	URIUniversalCommunication *URIUniversalCommunication `xml:"ram:URIUniversalCommunication>ram:URIID"`
+	SpecifiedTaxRegistration  *SpecifiedTaxRegistration  `xml:"ram:SpecifiedTaxRegistration>ram:ID"`
+}
+
+// Contact defines the structure of the DefinedTradeContact of the CII standard
+type Contact struct {
+	Name  string `xml:"ram:PersonName"`
+	Phone string `xml:"ram:TelephoneUniversalCommunication>ram:CompleteNumber"`
+	Email string `xml:"ram:EmailURIUniversalCommunication>ram:URIID"`
+}
+
+// SpecifiedTaxRegistration defines the structure of the SpecifiedTaxRegistration of the CII standard
+type SpecifiedTaxRegistration struct {
+	ID       string `xml:",chardata"`
+	SchemeID string `xml:"schemeID,attr"`
+}
+
+// LegalOrganization defines the structure of the SpecifiedLegalOrganization of the CII standard
+type LegalOrganization struct {
+	ID   string `xml:"ram:ID"`
+	Name string `xml:"ram:TradingBusinessName"`
+}
+
+// Buyer defines the structure of the BuyerTradeParty of the CII standard
+type Buyer struct {
+	ID                        string                     `xml:"ram:ID"`
+	Name                      string                     `xml:"ram:Name"`
+	PostalTradeAddress        *PostalTradeAddress        `xml:"ram:PostalTradeAddress"`
+	URIUniversalCommunication *URIUniversalCommunication `xml:"ram:URIUniversalCommunication>ram:URIID"`
+}
+
+// PostalTradeAddress defines the structure of the PostalTradeAddress of the CII standard
+type PostalTradeAddress struct {
+	Postcode  string `xml:"ram:PostcodeCode"`
+	LineOne   string `xml:"ram:LineOne"`
+	City      string `xml:"ram:CityName"`
+	CountryID string `xml:"ram:CountryID"`
+}
+
+// URIUniversalCommunication defines the structure of URIUniversalCommunication of the CII standard
+type URIUniversalCommunication struct {
+	URIID    string `xml:",chardata"`
+	SchemeID string `xml:"schemeID,attr"`
+}
+
+// Delivery defines the structure of ApplicableHeaderTradeDelivery of the CII standard
+type Delivery struct {
+	Event *Date `xml:"ram:ActualDeliverySupplyChainEvent>ram:OccurrenceDateTime>udt:DateTimeString,omitempty"`
+}
+
+// Settlement defines the structure of ApplicableHeaderTradeSettlement of the CII standard
+type Settlement struct {
+	Currency              string   `xml:"ram:InvoiceCurrencyCode"`
+	TypeCode              string   `xml:"ram:SpecifiedTradeSettlementPaymentMeans>ram:TypeCode"`
+	PayeeFinancialAccount string   `xml:"ram:SpecifiedTradeSettlementPaymentMeans>ram:PayeePartyCreditorFinancialAccount>ram:IBANID"`
+	Tax                   *Tax     `xml:"ram:ApplicableTradeTax"`
+	Description           string   `xml:"ram:SpecifiedTradePaymentTerms>ram:Description"`
+	Summary               *Summary `xml:"ram:SpecifiedTradeSettlementHeaderMonetarySummation"`
+}
+
+// Tax defines the structure of ApplicableTradeTax of the CII standard
+type Tax struct {
+	CalculatedAmount      string `xml:"ram:CalculatedAmount"`
+	TypeCode              string `xml:"ram:TypeCode"`
+	BasisAmount           string `xml:"ram:BasisAmount"`
+	CategoryCode          string `xml:"ram:CategoryCode"`
+	RateApplicablePercent string `xml:"ram:RateApplicablePercent"`
+}
+
+// Summary defines the structure of SpecifiedTradeSettlementHeaderMonetarySummation of the CII standard
+type Summary struct {
+	TotalAmount         string          `xml:"ram:LineTotalAmount"`
+	TaxBasisTotalAmount string          `xml:"ram:TaxBasisTotalAmount"`
+	TaxTotalAmount      *TaxTotalAmount `xml:"ram:TaxTotalAmount"`
+	GrandTotalAmount    string          `xml:"ram:GrandTotalAmount"`
+	DuePayableAmount    string          `xml:"ram:DuePayableAmount"`
+}
+
+// TaxTotalAmount defines the structure of the TaxTotalAmount of the CII standard
+type TaxTotalAmount struct {
+	Amount   string `xml:",chardata"`
+	Currency string `xml:"currencyID,attr"`
+}
+
+// NewTransaction creates the transaction part of a EN 16931 compliant invoice
+func NewTransaction() *Transaction {
+	lines := []*Line{
+		{
+			ID:       "Zeitschrift [...]",
+			Name:     "Zeitschrift [...]",
+			NetPrice: "288.79",
+			TradeDelivery: &Quantity{
+				Amount:   "1",
+				UnitCode: "XPP",
+			},
+			TradeSettlement: &TradeSettlement{
+				TaxType:        "VAT",
+				TaxCode:        "S",
+				TaxRatePercent: "7",
+				Sum:            "288.79",
+			},
+		},
+		{
+			ID:       "Porto + Versandkosten",
+			Name:     "Porto + Versandkosten",
+			NetPrice: "26.07",
+			TradeDelivery: &Quantity{
+				Amount:   "1",
+				UnitCode: "XPP",
+			},
+			TradeSettlement: &TradeSettlement{
+				TaxType:        "VAT",
+				TaxCode:        "S",
+				TaxRatePercent: "7",
+				Sum:            "26.07",
+			},
+		},
+	}
+	agreement := &Agreement{
+		BuyerReference: "04011000-12345-03",
+		Seller: &Seller{
+			Name: "[Seller name]",
+			LegalOrganization: &LegalOrganization{
+				ID:   "[HRA-Eintrag]",
+				Name: "[Seller trading name]",
+			},
+			Contact: &Contact{
+				Name:  "nicht vorhanden",
+				Phone: "+49 1234-5678",
+				Email: "seller@email.de",
+			},
+			PostalTradeAddress: &PostalTradeAddress{
+				Postcode:  "12345",
+				LineOne:   "[Seller address line 1]",
+				City:      "[Seller city]",
+				CountryID: "DE",
+			},
+			URIUniversalCommunication: &URIUniversalCommunication{
+				URIID:    "seller@email.de",
+				SchemeID: "EM",
+			},
+			SpecifiedTaxRegistration: &SpecifiedTaxRegistration{
+				ID:       "DE 123456789",
+				SchemeID: "VA",
+			},
+		},
+		Buyer: &Buyer{
+			ID:   "[Buyer identifier]",
+			Name: "[Buyer name]",
+			PostalTradeAddress: &PostalTradeAddress{
+				Postcode:  "12345",
+				LineOne:   "[Buyer address line 1]",
+				City:      "[Buyer city]",
+				CountryID: "DE",
+			},
+			URIUniversalCommunication: &URIUniversalCommunication{
+				URIID:    "buyer@info.de",
+				SchemeID: "EM",
+			},
+		},
+	}
+	return &Transaction{
+		Lines:     lines,
+		Agreement: agreement,
+		Delivery: &Delivery{
+			Event: &Date{
+				Date:   "20160621",
+				Format: "102",
+			},
+		},
+		Settlement: &Settlement{
+			Currency:              "EUR",
+			TypeCode:              "58",
+			PayeeFinancialAccount: "DE75512108001245126199",
+			Description:           "Zahlbar sofort ohne Abzug.",
+			Tax: &Tax{
+				CalculatedAmount:      "22.04",
+				TypeCode:              "VAT",
+				BasisAmount:           "314.86",
+				CategoryCode:          "S",
+				RateApplicablePercent: "7",
+			},
+			Summary: &Summary{
+				TotalAmount:         "314.86",
+				TaxBasisTotalAmount: "314.86",
+				GrandTotalAmount:    "336.9",
+				DuePayableAmount:    "336.9",
+				TaxTotalAmount: &TaxTotalAmount{
+					Amount:   "22.04",
+					Currency: "EUR",
+				},
+			},
+		},
+	}
+}
--- xinvoice.go
+++ xinvoice.go
@@ -7,15 +7,77 @@ import (
 	"github.com/invopop/gobl"
 )
 
+// CFDI schema constants
+const (
+	RSM              = "urn:un:unece:uncefact:data:standard:CrossIndustryInvoice:100"
+	RAM              = "urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:100"
+	QDT              = "urn:un:unece:uncefact:data:standard:QualifiedDataType:100"
+	UDT              = "urn:un:unece:uncefact:data:standard:UnqualifiedDataType:100"
+	BusinessProcess  = "urn:fdc:peppol.eu:2017:poacc:billing:01:1.0"
+	GuidelineContext = "urn:cen.eu:en16931:2017#compliant#urn:xeinkauf.de:kosit:xrechnung_3.0"
+)
+
 // Document is a pseudo-model for containing the XML document being created
 type Document struct {
-	XMLName xml.Name `xml:"rsm:CrossIndustryInvoice"`
+	XMLName                xml.Name           `xml:"rsm:CrossIndustryInvoice"`
+	RSMNamespace           string             `xml:"xmlns:rsm,attr"`
+	RAMNamespace           string             `xml:"xmlns:ram,attr"`
+	QDTNamespace           string             `xml:"xmlns:qdt,attr"`
+	UDTNamespace           string             `xml:"xmlns:udt,attr"`
+	BusinessProcessContext string             `xml:"rsm:ExchangedDocumentContext>ram:BusinessProcessSpecifiedDocumentContextParameter>ram:ID"`
+	GuidelineContext       string             `xml:"rsm:ExchangedDocumentContext>ram:GuidelineSpecifiedDocumentContextParameter>ram:ID"`
+	ExchangedDocument      *ExchangedDocument `xml:"rsm:ExchangedDocument"`
+	Transaction            *Transaction       `xml:"rsm:SupplyChainTradeTransaction"`
+}
+
+// ExchangedDocument a collection of data for a Cross Industry Invoice Header that is exchanged between two or more parties in written, printed or electronic form.
+type ExchangedDocument struct {
+	ID           string `xml:"ram:ID"`
+	TypeCode     string `xml:"ram:TypeCode"`
+	IssueDate    *Date  `xml:"ram:IssueDateTime>udt:DateTimeString"`
+	IncludedNote *Note  `xml:"ram:IncludedNote"`
+}
+
+// Date defines date in the UDT structure
+type Date struct {
+	Date   string `xml:",chardata"`
+	Format string `xml:"format,attr,omitempty"`
+}
+
+// Note defines note in the RAM structure
+type Note struct {
+	Content     string `xml:"ram:Content"`
+	SubjectCode string `xml:"ram:SubjectCode"`
 }
 
 // NewDocument converts a GOBL envelope into a XRechnung and Factur-X document
 func NewDocument(_ *gobl.Envelope) (*Document, error) {
-	doc := Document{}
+	doc := Document{
+		RSMNamespace:           RSM,
+		RAMNamespace:           RAM,
+		QDTNamespace:           QDT,
+		UDTNamespace:           UDT,
+		BusinessProcessContext: BusinessProcess,
+		GuidelineContext:       GuidelineContext,
+		ExchangedDocument:      newHeader(),
+		Transaction:            NewTransaction(),
+	}
 	return &doc, nil
+}
+
+func newHeader() *ExchangedDocument {
+	return &ExchangedDocument{
+		ID:       "123456XX",
+		TypeCode: "380",
+		IssueDate: &Date{
+			Date:   "20160404",
+			Format: "102",
+		},
+		IncludedNote: &Note{
+			Content:     "Es gelten unsere Allgem. Geschäftsbedingungen, die Sie unter […] finden.",
+			SubjectCode: "ADU",
+		},
+	}
 }
 
 // Bytes returns the XML representation of the document in bytes
